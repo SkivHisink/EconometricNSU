434cbadacaefc1fa2f9309a8fcfae3b7948446c7
khorunzhenko <khorunzhenko.as@petrogm.com>
2022-12-20 10:51:51 +0700

    task 13 is done

--- Task13/Task13.r
+++ Task13/Task13.r
@@ -33,8 +33,8 @@ ep <- test_ts - yp
 plot(ep, type = "o", pch = 18, ylim = range(ep, 0))
 abline(h = 0, col = "#00FF00")
 
-bottom_boundary <- pr$pred - pr$se * qnorm(1 - 0.1 / 2)
-top_boundary <- pr$pred + pr$se * qnorm(1 - 0.1 / 2)
+bottom_boundary <- pr$pred - pr$se * qnorm(1 - 0.1)
+top_boundary <- pr$pred + pr$se * qnorm(1 - 0.1)
 
 str(bottom_boundary)
 str(top_boundary)
@@ -46,118 +46,68 @@ lines(yp, col = "blue", type = "o", pch = 18, lty = 2)
 lines(bottom_boundary, col = "#FF0000", lty = 2, lwd = 2)
 lines(top_boundary, col = "#00FF00", lty = 2, lwd = 2)
 
-#-------------------------------------------------
-df <- read.delim("Turkey_XU100.TSV")
+#Task2
+df <- read.delim("US_sp500.csv", sep = ",")
 View(df)
-
+df$Date <- rev(df$Date)
+df$Date <- as.Date(df$Date, "%Y-%m-%d")
+df$Open <- rev(df$Open)
+df$Close <- rev(df$Close)
+df$High <- rev(df$High)
+df$Low <- rev(df$Low)
+df$Volume <- rev(df$Volume)
+df[Adj.Close] <- rev(df[Adj.Close])
+# Рассчитайте логарифмические доходности r=diff log * 100
 r <- diff(log(df$Close)) * 100
 plot.ts(r)
 grid()
-abline(h = 0, col = 4)
-
+abline(h = 0, col = "#FF0000")
+# Постройте график АКФ для r^2 (квадратов доходностей)
 acf(r^2)
+# и |r| (модулей доходностей)
 acf(abs(r))
-
-TT <- length(r)
+# Рассчитайте волатильность по методу Riskmetrics *, при λ=0.95. Постройте график доходностей и ±волатильности.
+# (* см. http://uu.diva-portal.org/smash/get/diva2:304496/FULLTEXT01.pdf формула (30), p. 14)
+data_len <- length(df$Close)
 lambda <- 0.95
 s2 <- c(1)
-for (t in 2:TT){
-  s2[t] <- lambda * s2[t-1] + (1 - lambda) * r[t-1]^2
+for (t in 2:data_len){
+  s2[t] <- lambda * s2[t - 1] + (1 - lambda) * r[t - 1]^2
 }
 vol.rm <- sqrt(s2)
 
 plot.ts(vol.rm, ylim = range(vol.rm, 0))
-abline(h=0, col = 6, lwd = 2)
+abline(h = 0, col = "#FF0000", lwd = 2)
 
 plot.ts(r)
-lines(vol.rm, col="blue", lwd=2)
-lines(-vol.rm, col="blue", lwd=2)
-abline(h=0, col=4, lwd=2)
-
+lines(vol.rm, col = "#0000FF", lwd = 3)
+lines(-vol.rm, col = "#ff0000", lwd = 3)
+abline(h = 0, col = "#00ff00", lwd = 2)
 
 library(fGarch)
-ga <- fGarch::garchFit(~ garch(1,1),r,
+ga <- fGarch::garchFit(~ garch(1, 1), r,
                        include.mean = FALSE,
                        cond.dist = "std",
-                       trace=FALSE)
+                       trace = FALSE)
 print(ga)
 
 vol.ga <- volatility(ga)
 
-plot.ts(vol.ga, ylim=range(vol.ga,0))
-abline(h=0,col=6, lwd=2)
+plot.ts(vol.ga, ylim = range(vol.ga, 0))
+abline(h = 0, col = "#FF0000", lwd = 2)
 
 plot.ts(r)
-lines(vol.ga, col="blue", lwd=2)
-lines(-vol.ga, col="blue", lwd=2)
-abline(h=0, col=4,lwd=2)
+lines(vol.ga, col = "#0000FF", lwd = 3)
+lines(-vol.ga, col = "#ff0000", lwd = 3)
+abline(h = 0, col = "#00ff00", lwd = 2)
 
-pr <- predict(ga, n.ahead=100)
-plot(pr$standardDeviation)
+pred <- predict(ga, n.ahead = 100)
+plot(pred$standardDeviation)
+#volatility predict = volp
+volp <- c(vol.ga, pred$standardDeviation)
 
-volp <- c(vol.ga, pr$standardDeviation)
-
-plot.ts(volp, xlim = c(1600,TT+100), col = 6,ylim=range(volp,0))
-abline(v=TT, lty=3)
-lines(vol.ga)
-abline(h=0, col=4, lwd=2)
-
-#--
-library(psych)
-library(lmtest)
-setwd("C:/Users/Legion/Desktop/шлак/R_datas")
-df = read.csv('Chile_IPSA.csv')
-View(df)
-yield <- diff(log(df$Close))*100
-
-plot.ts(yield)
-grid()
-abline(h=0,col=2)
-
-acf(yield^2)
-acf(abs(yield))
-
-TT <- length(yield)
-#Riskmetrics
-s2 <- c(1)
-lam <- 0.95
-for(i in 2:TT)
-  s2[i] <- lam*s2[i-1]+yield[i-1]^2*(1-lam)
-  
-vol.rm <- sqrt(s2)
-
-plot.ts(vol.rm, ylim=range(vol.rm,0))  
-abline(h=0,col=2)
-grid()
-lines(vol.rm,col="green2")
-
-
-plot.ts(yield)  
-abline(h=0,col=2)
-grid()
-lines(vol.rm,col="green2")
-lines(-vol.rm,col="green2")
-
-library(fGarch)
-g_mod <- garchFit(~garch(1,1),yield,trace=F,include.mean = F,cond.dist = "std")#std - normalized Student
-print(g_mod)
-
-vol.g_mod <- volatility(g_mod)
-
-plot.ts(yield)  
-abline(h=0,col=2)
-grid()
-lines(vol.g_mod,col="green2")
-lines(-vol.g_mod,col="green2")
-
-pred <- predict(g_mod,n.ahed=100)
-
-plot(pred$standardDeviation)#стратовали из периода высокой волатильности
-
-volp=c(vol.g_mod,pred$standardDeviation)
-length(vol.g_mod)
-TT
-plot.ts(volp)
-lines(volp[TT:TT+100],col=2,pch=18)
-abline(v=TT,col=2)
-
+plot.ts(volp, xlim = c(12500, data_len + 100),
+col = "#000000", ylim = c(0, 3))
+abline(v = TT, lty = 3)
+lines(vol.ga, col = "#ff0000")
+abline(h = 0, col = "#00ff00", lwd = 2)
